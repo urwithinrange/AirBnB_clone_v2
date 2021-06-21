7de398ef4f40cb9a4ce3f2afa35391ad36ab518a
Derek_Clemens <urwithinrange@yahoo.com>
2021-06-21 15:27:55 +0000

    pep8

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,15 +5,18 @@ python3 flask run
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
     """return hello HBNB"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hello_v2():
     """return only HBNB"""
-    return "HBNB"    
+    return "HBNB"
+
 
 @app.route("/C/<text>", strict_slashes=False)
 def c_text():
