36fb92422a4862812dd9111ecbbd968cfdc71316
Derek_Clemens <urwithinrange@yahoo.com>
2021-06-21 15:38:47 +0000

    pep8

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -5,20 +5,24 @@ python3 flask run
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
     """return hello HBNB"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hello_v2():
     """return only HBNB"""
-    return "HBNB"    
+    return "HBNB"
+
 
 @app.route("/C/<text>", strict_slashes=False)
 def c_text():
     """return C followed by a stream"""
     return "C {}".format(text.replace('_', ' '))
+
 
 @app.route("/python/", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -5,26 +5,31 @@ python3 flask run
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
     """return hello HBNB"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hello_v2():
     """return only HBNB"""
-    return "HBNB"    
+    return "HBNB"
+
 
 @app.route("/C/<text>", strict_slashes=False)
 def c_text():
     """return C followed by a stream"""
     return "C {}".format(text.replace('_', ' '))
 
+
 @app.route("/python/", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python_text(text="is cool"):
     """return Python is <text>"""
     return "Python {}".format(text.replace('_', ' '))
+
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def gimmenumbers(n):
