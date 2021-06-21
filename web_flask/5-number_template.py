dda17f978a77902318f1e05712194016a54e7783
Derek_Clemens <urwithinrange@yahoo.com>
2021-06-21 15:40:19 +0000

    pep8

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -6,20 +6,24 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
     """return hello HBNB"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hello_v2():
     """return only HBNB"""
-    return "HBNB"    
+    return "HBNB"
+
 
 @app.route("/C/<text>", strict_slashes=False)
 def c_text():
     """return C followed by a stream"""
     return "C {}".format(text.replace('_', ' '))
+
 
 @app.route("/python/", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
@@ -27,10 +31,12 @@ def python_text(text="is cool"):
     """return Python is <text>"""
     return "Python {}".format(text.replace('_', ' '))
 
+
 @app.route("/number/<int:n>", strict_slashes=False)
 def gimmenumbers(n):
     """return number only if an int"""
     return "{} is a number".format(n)
+
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def gimmenumtemplate(n):
